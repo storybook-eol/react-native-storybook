1ac4c083bd6fb507edb6b92030675ad1961b1aa1
Muhammed Thanish <mnmtanish@gmail.com>
2016-06-23 11:53:40 +0530

    Support Addons

--- src/client/index.js
+++ src/client/index.js
@@ -5,6 +5,7 @@ import { stories } from './configure';
 // export configuration API functions
 export { configure } from './configure/';
 export { storiesOf } from './configure/';
+export { setAddon } from './configure/';
 
 // export the function to generate the preview component
 export function PreviewComponent({port, host = 'localhost'}) {
